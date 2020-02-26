45037d169e54d7b4ba386c365bdd5069a58a1e90
luca blanc <luca@momenthor.fr>
2020-02-26 13:34:42 +0100

    Up app.js

--- App/App.js
+++ App/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Top from './component/topbar';
-import {View, Image} from 'react-native';
+import {StyleSheet, View, Image} from 'react-native';
 
 export default class App extends Component {
   render() {
@@ -17,12 +17,77 @@ export default class App extends Component {
           </View>
 
           {/*boxUser*/}
-          <View>
-            <Image
-              source={}
-            />
+          <View style={{flex: 1,alignItems: 'center'}}>
+            <View style={styles.userBox}>
+                <View style={styles.shadow}><Image
+                  style={styles.userImg}
+                  source={{uri: 'https://i.goopics.net/y2OdD.png'}}
+                  resizeMode="cover"
+                /></View>
+                <View style={styles.userPrix}>
+
+                </View>
+                <View style={styles.userInfo}>
+                  
+                </View>
+            </View>
           </View> 
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  userBox: {
+      backgroundColor: 'white',
+      marginTop: 50,
+      width: 345,
+      height: 390,
+      alignItems: 'center',
+  },
+
+  userImg: {
+    width: 320,
+    height: 360,
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: '#9bc3e2',
+  },
+
+  userPrix: {
+    width: 100,
+    height: 20,
+    backgroundColor: 'black',
+    position: 'relative',
+    top: -45,
+  },
+
+  shadow:{
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 0,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 5,
+  },
+
+  userInfo: {
+    width: 340,
+    height: 80,
+    backgroundColor: 'white',
+    opacity: 0.74,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 0,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 5,
+
+    position: 'relative',
+    top: -65,
+    borderRadius: 5,
+  }
+  
+})
